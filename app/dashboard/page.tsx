2639a56216c2e5140397dd11530cdb25e375221b
leyla_qiao <775177772@qq.com>
2025-02-27 00:26:27 +0800

    init repo

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Dashboard;
